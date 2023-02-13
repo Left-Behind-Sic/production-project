16957565aacc731188df55e57a39dd81f21cf12e
Left-Behind-Sic <dushaster@gmail.com>
2023-02-13 20:42:21 +0500

    error boundary

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,25 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button } from 'shared/ui/Button/Button';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+// компонент для тестирования Error Boundary
+export const BugButton = () => {
+  const [error, setError] = useState(false);
+
+  const { t } = useTranslation();
+
+  const throwError = () => setError(true);
+
+  useEffect(() => {
+    if (error) throw new Error();
+  }, [error]);
+
+  return (
+    <Button
+      onClick={throwError}
+    >
+      {t('throw error')}
+    </Button>
+  );
+};
