0d689c4a714c6c37fbf0c34f44a9bfa5242eec47
Left-Behind-Sic <dushaster@gmail.com>
2023-04-24 14:34:46 +0500

    fixed storybook

--- src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.stories.tsx
+++ src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.stories.tsx
@@ -2,16 +2,50 @@ import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import { AvatarDropdown } from './AvatarDropdown';
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
+import { UserRole } from '@/entities/User';
 
 export default {
-  title: 'shared/AvatarDropdown',
+  title: 'features/AvatarDropdown',
   component: AvatarDropdown,
   argTypes: {
     backgroundColor: { control: 'color' },
   },
+  decorators: [
+    (Story) => (
+      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <Story />
+      </div>
+    ),
+  ],
 } as ComponentMeta<typeof AvatarDropdown>;
 
 const Template: ComponentStory<typeof AvatarDropdown> = (args) => <AvatarDropdown {...args} />;
 
-export const Normal = Template.bind({});
-Normal.args = {};
+export const Admin = Template.bind({});
+Admin.args = {};
+Admin.decorators = [
+  StoreDecorator({
+    user: {
+      authData: {
+        id: '1',
+        avatar: 'https://xakep.ru/wp-content/uploads/2018/05/171485/KuroiSH-hacker.jpg',
+        roles: [UserRole.ADMIN],
+      },
+    },
+  }),
+];
+
+export const User = Template.bind({});
+User.args = {};
+User.decorators = [
+  StoreDecorator({
+    user: {
+      authData: {
+        id: '1',
+        avatar: 'https://xakep.ru/wp-content/uploads/2018/05/171485/KuroiSH-hacker.jpg',
+        roles: [UserRole.USER],
+      },
+    },
+  }),
+];
--- src/features/notificationButton/ui/NotificationButton/NotificationButton.stories.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.stories.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
+import withMock from 'storybook-addon-mock';
 import { NotificationButton } from './NotificationButton';
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 
 export default {
-  title: 'shared/NotificationButton',
+  title: 'features/NotificationButton',
   component: NotificationButton,
   argTypes: {
     backgroundColor: { control: 'color' },
   },
+  decorators: [
+    withMock,
+    (Story) => (
+      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <Story />
+      </div>
+    ),
+  ],
 } as ComponentMeta<typeof NotificationButton>;
 
 const Template: ComponentStory<typeof NotificationButton> = (args) => (
@@ -17,3 +27,30 @@ const Template: ComponentStory<typeof NotificationButton> = (args) => (
 
 export const Normal = Template.bind({});
 Normal.args = {};
+Normal.decorators = [StoreDecorator({})];
+Normal.parameters = {
+  mockData: [
+    {
+      url: `${__API__}/notifications`,
+      method: 'GET',
+      status: 200,
+      response: [
+        {
+          id: '1',
+          title: 'уведомление',
+          description: 'описание',
+        },
+        {
+          id: '2',
+          title: 'уведомление 2',
+          description: 'описание',
+        },
+        {
+          id: '3',
+          title: 'уведомление 3',
+          description: 'описание',
+        },
+      ],
+    },
+  ],
+};
