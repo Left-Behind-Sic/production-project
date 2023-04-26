7b03d58395ada34133c03805a6de5eefabf9d4e3
Left-Behind-Sic <dushaster@gmail.com>
2023-04-26 22:04:39 +0500

    fix unit tests, update screens

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -1,10 +1,10 @@
+export { ArticleDetails } from './ui/ArticleDetails/ArticleDetails';
+
 export {
   getArticleDetailsData,
   getArticleDetailsError,
   getArticleDetailsIsLoading,
 } from './model/selectors/articleDetails';
-
-export { ArticleDetails } from './ui/ArticleDetails/ArticleDetails';
 
 export type { Article } from './model/types/article';
 
