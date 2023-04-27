11a7f5816116884356fec328bb8f03745ebc49d9
Left-Behind-Sic <dushaster@gmail.com>
2023-04-27 15:24:02 +0500

    upgrade webpack config, add babel cache, clear cache script after npm i

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -19,18 +19,13 @@ export function buildPlugins({
       template: paths.html,
     }),
     new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].[contenthash:8].css',
-      chunkFilename: 'css/[name].[contenthash:8].css',
-    }),
+
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
       __API__: JSON.stringify(apiUrl),
       __PROJECT__: JSON.stringify(project),
     }),
-    new CopyPlugin({
-      patterns: [{ from: paths.locales, to: paths.buildLocales }],
-    }),
+
     new CircularDependencyPlugin({
       exclude: /node_modules/,
       failOnError: true,
@@ -52,6 +47,18 @@ export function buildPlugins({
     plugins.push(
       new BundleAnalyzerPlugin({
         openAnalyzer: false,
+      }),
+    );
+  }
+
+  if (!isDev) {
+    plugins.push(
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].[contenthash:8].css',
+        chunkFilename: 'css/[name].[contenthash:8].css',
+      }),
+      new CopyPlugin({
+        patterns: [{ from: paths.locales, to: paths.buildLocales }],
       }),
     );
   }
